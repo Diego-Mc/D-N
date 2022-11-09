debaccb7cf14fa0eac2b2396e8dd17feef8bf4a1
Diego Mc <projkd1@gmail.com>
2022-11-09 12:04:10 +0200

    Initial commit

---
+++ routes.js
@@ -0,0 +1,60 @@
+import homePage from './views/app-home.cmp.js'
+import aboutPage from './views/app-about.cmp.js'
+
+const { createRouter, createWebHashHistory } = VueRouter
+
+const routerOptions = {
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: '/',
+      component: homePage,
+    },
+    {
+      path: '/about',
+      component: aboutPage,
+    },
+    {
+      path: '/maily',
+      component: mailyApp,
+      children: [
+        {
+          path: 'compose/:id?',
+          component: emailPreview,
+        },
+        {
+          path: ':id',
+          component: emailPreview,
+        },
+      ],
+    },
+    {
+      path: '/keepy',
+      component: keepyApp,
+      children: [
+        {
+          path: ':id',
+          component: noteEdit,
+        },
+      ],
+    },
+    {
+      path: '/booky',
+      component: bookyApp,
+      children: [
+        {
+          path: ':id',
+          component: bookDetails,
+          children: [
+            {
+              path: 'review/:id',
+              component: reviewPreview,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
+
+export const router = createRouter(routerOptions)
---
+++ services/util.service.js
@@ -0,0 +1,25 @@
+export const utilService = {
+  saveToStorage,
+  loadFromStorage,
+  makeId,
+}
+
+function saveToStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value))
+}
+
+function loadFromStorage(key) {
+  return JSON.parse(localStorage.getItem(key))
+}
+
+//TODO: util for params
+
+function makeId(length = 5) {
+  var txt = ''
+  var possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  for (var i = 0; i < length; i++) {
+    txt += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return txt
+}
